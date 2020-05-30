5b0615d9549603114c18a45fb74406e558016dcd
MoAnd <moande15@student.aau.dk>
2020-05-30 20:49:14 +0200

    Libraries

---
+++ Libraries.R
@@ -0,0 +1,46 @@
+if (!require("pacman")) install.packages("pacman")
+
+# Tidyverse ---------------------------------------------------------------
+pacman::p_load(magrittr)
+pacman::p_load(dplyr)
+pacman::p_load(tidyr)
+pacman::p_load(purrr)
+pacman::p_load(readr)
+pacman::p_load(tibble)
+pacman::p_load(lubridate)
+pacman::p_load(stringr)
+pacman::p_load(ggplot2)
+pacman::p_load(purrr)
+pacman::p_load(glue)
+pacman::p_load(tidyverse)
+
+
+# Time series -------------------------------------------------------------
+pacman::p_load(skellam)
+pacman::p_load(zoo)
+pacman::p_load(xts)
+pacman::p_load(janitor)
+pacman::p_load(sde)
+
+
+# Machine Learning / Statistics -------------------------------------------
+pacman::p_load(vcd)
+pacman::p_load(Countr)
+pacman::p_load(copula)
+pacman::p_load(rpgm)
+
+
+# Graphics ----------------------------------------------------------------
+pacman::p_load(dygraphs)
+pacman::p_load(plotly)
+pacman::p_load(ggformula)
+pacman::p_load(kableExtra)
+
+
+# Other -------------------------------------------------------------------
+pacman::p_load(parallel)
+pacman::p_load(foreach)
+pacman::p_load(doParallel)
+pacman::p_load(tictoc)
+
+
